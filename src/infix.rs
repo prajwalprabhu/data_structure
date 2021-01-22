a5e1088a6627c44c39a9636c13def6db32a0af73
T Prajwal Prabhu <prajwalprabhu.tellar@gmail.com>
2021-01-22 21:30:57 +0530

    "Added ability to solve power '^'"

--- src/infix.rs
+++ src/infix.rs
@@ -1,7 +1,7 @@
 //Solve Post fix expression
 fn to_result(result: Vec<char>) -> u32 {
     let mut stack: Vec<u32> = Vec::new(); //Stack
-    let operator = "*/+-";
+    let operator = "*/+-%^";
     for i in result {
         if operator.contains(i) {
             let a = stack.pop().unwrap();
@@ -11,6 +11,7 @@ fn to_result(result: Vec<char>) -> u32 {
                 '-' => stack.push(b - a),
                 '/' => stack.push(b / a),
                 '*' => stack.push(b * a),
+                '^' => stack.push(b.pow(a)),
                 _ => panic!("Something went wrong"),
             }
         } else {
@@ -19,11 +20,41 @@ fn to_result(result: Vec<char>) -> u32 {
     }
     stack.pop().unwrap()
 }
+fn check(eq: String) -> u32 {
+    let mut eq: Vec<char> = eq.chars().collect();
+    let mut result: u32;
+    if eq.contains(&'^') {
+        for i in 0..eq.len() - 1 {
+            if eq[i] == '^' {
+                let a: char = eq[i - 1];
+                let b: char = eq[i + 1];
+                let a: u32 = a.to_digit(10).unwrap();
+                let b: u32 = b.to_digit(10).unwrap();
+                result = a.pow(b);
+                let result = format!("{}", result);
+                // println!("{}",result);
+                println!("Checked {:?}", eq);
+                eq.remove(i - 1);
+                eq.remove(i - 1);
+                eq.remove(i - 1);
+                // println!("Checked {:?}",eq);
+                eq.insert(i - 1, result.chars().next().unwrap());
+            }
+        }
+        // println!("Checked {:?}",eq);
+    }
+    let mut result = String::new();
+    eq.iter().for_each(|i| {
+        result.push(*i);
+    });
+    // let a = String::from_iter(eq.iter());
+    to_result(to_post(result))
+}
 //Program to Convert Infix to Postfix Expression
 fn to_post(eq: String) -> Vec<char> {
     let mut stack: Vec<char> = Vec::new(); //Stack
     let mut output: Vec<char> = Vec::new(); //Output
-    let operator = "*/+-";
+    let operator = "*/+-^";
     for i in eq.chars() {
         if i == '(' {
             stack.push(i);
@@ -49,6 +80,7 @@ fn to_post(eq: String) -> Vec<char> {
                 }
             }
         } else if i == ')' {
+            // println!("{}",eq.Index(i));
             while stack.len() != 0 {
                 let a = stack.pop().unwrap();
                 if a == '(' {
@@ -68,11 +100,12 @@ fn to_post(eq: String) -> Vec<char> {
     output
 }
 pub fn run() {
-    let infix = String::from("2*3/(2-1)+5*3");
-    let result = to_post(infix.clone());
-    println!(" Infix = {:?},PostFix = {:?}", infix, result);
-    println!("Result = {:?}", to_result(result));
-    let infix = String::from("(A+B)*C-(D-E)*(F+G)");
-    let result = to_post(infix.clone());
-    println!(" Infix = {:?},PostFix = {:?}", infix, result);
+    let infix = String::from("2*3/(4-1)+5*3^2");
+    // let result = to_post(infix.clone());
+    println!("infix = {:?}\nPostfix = {:?}",infix, check(infix.clone()));
+    // println!(" Infix = {:?},PostFix = {:?}", infix, result);
+    // println!("Result = {:?}", to_result(result));
+    // let infix = String::from("(A+B)*C-(D-E)*(F+G)");
+    // let result = to_post(infix.clone());
+    // println!(" Infix = {:?},PostFix = {:?}", infix, result);
 }
