c2a0c5afed2b79d9f1d68f631651c03a9eccfc0d
T Prajwal Prabhu <prajwalprabhu.tellar@gmail.com>
2021-02-05 19:48:10 +0530

    "test2"

--- push.sh
+++ push.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-git add -A;
-git commit -a;
-git checkout main;
-git merge test ;
-git push origin;
+git add -A &&
+git commit -a &&
+git checkout main &&
+git merge test &&
+git push origin &&
+git checkout test 
+ 
